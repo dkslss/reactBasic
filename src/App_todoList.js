ea6c508c06df8cc2a8f0de20b29dc9aad71a55e8
iyhan <gksdyd0306@naver.com>
2025-04-01 22:15:03 +0900

    메시지

---
+++ src/App_todoList.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if(toDo === "") {
+      return;
+    } 
+    
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo(""); 
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value= {toDo}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <button>Add to do</button>
+      </form>
+    <hr>
+    </hr>
+    <ul>
+    {toDos.map((item, index) => (
+      <li key={index}>{item}</li>
+      ))}
+    </ul>
+    </div>
+  );
+}
+
+export default App;
